9a3b4c4468145a7cb8201e1d889bcc650bd6e4c5
alanwjunior <wilckayalan@gmail.com>
2019-01-07 17:05:37 -0200

    initialize blip service package
    Developped the blip service package responsible for get the published flow and update flow.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-index.js
+global.atob = require('atob')
+global.btoa = require('btoa')
+
+const blipApi = require('./integrations/blip-integration/blipservice')
+
+const blipService = blipApi.BlipService('RUQxbE5JVHN4Y2tkOC9FZnBmOCsxZVg2RjNnSHNnRUVSa0RKRXljVTlBZytqQ1NEc2ZtM3VnTFpxUVFmeWVYM0o0T3ppNFhlWmZ1cXVQam9UaVJWT3c9PQ==')
+
+blipService.getPublishedFlow('clubeitacolomy')
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error.message))
+    
+console.log('teste')
--- integrations/blip-integration/blipservice.js
+++ integrations/blip-integration/blipservice.js
@@ -1,27 +1,52 @@
+const BlipService = (blipUserToken) => {
+    const axios = require('axios')
 
-const createAxiosInstance = () => {
+    const userToken = blipUserToken
 
-};
-
-const createBlipAuthorizationToken = () => {
-
-};
-
-const getBotAccessKey = (botShortName) => {
-
-};
-
-const getPublishedFlow = () => {
-    let axios = require('axios')
-    const guid = require('./../../utils/guid-generator/guid')
-    axios.defaults.headers.common = { 'Authorization': 'Key ' + encodedAuthKey, 'Content-Type': 'application/json' }
-    axios.post('https://msging.net/' + 'commands', {
-        id: guid.newGuid(),
-        method: 'get',
-        uri: '/buckets/blip_portal:builder_published_flow'
-    })
-        .then(response => {
-            return response.data
+    const createAxiosInstance = (endPoint, token) => {
+        let axiosInstance = axios.create({
+            baseURL: endPoint
         })
-        .catch(error => console.log(error.message))
-};
+        axiosInstance.defaults.headers.common = { 'Authorization': + token, 'Content-Type': 'application/json' }
+        
+        return axiosInstance
+    }
+    
+    const createBlipAuthorizationToken = async (botShortName) => {
+        const botAccessKey = await getBotAccessKey(botShortName)
+        let authorizationKey = botShortName + ':' + botAccessKey
+        let encodedAuthKey = btoa(authorizationKey)
+        return encodedAuthKey
+    }
+    
+    const getBotAccessKey = async (botShortName) => {
+        try {
+            axios.defaults.headers.common = { 'Authorization': 'Token ' + userToken }
+            let response = await axios.get('https://api.blip.ai/applications/' + botShortName)
+            return atob(response.data.accessKey)
+        }
+        catch(error){
+            console.log(error.message)
+            throw error
+        }
+    }
+    
+    const getPublishedFlow = async (botShortName) => {
+        const botAuthKey = await createBlipAuthorizationToken(botShortName)
+        const guid = require('./../../utils/guid-generator/guid')
+        axios.defaults.headers.common = { 'Authorization': 'Key ' + botAuthKey, 'Content-Type': 'application/json' }
+        return axios.post('https://msging.net/' + 'commands', {
+            id: guid.newGuid(),
+            method: 'get',
+            uri: '/buckets/blip_portal:builder_published_flow'
+        })
+    }
+
+    return {
+        getPublishedFlow: getPublishedFlow,
+        userToken: userToken
+    }
+
+}
+
+module.exports.BlipService = BlipService
